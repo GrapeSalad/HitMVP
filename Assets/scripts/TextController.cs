88bebe78cce60f0e9c097439a0778f21086881a0
David Wilson <davidtheadmiral@gmail.com>
2017-09-04 16:25:55 -0700

    complete basic mvp, user hits play and ball drops to launch another ball through a wall. triggers, hinges, basic physics and grouping game-objects together.

---
+++ Assets/scripts/TextController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TextControl
+{
+	public class TextController : MonoBehaviour {
+
+		public Text text;
+		private enum States {play, win};
+		private States myState;
+
+		// Use this for initialization
+		void Start () {
+			myState = States.play;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (myState == States.play) {
+				state_space ();
+			}else if(myState == States.win){
+				state_enter ();
+			}
+		}
+		public void state_space()
+		{
+			text.text = "Launch The Ball!";
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				myState = States.win;
+			}
+		}
+		void state_enter()
+		{
+			text.text = "Press Enter";
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				myState = States.play;
+			}
+		}
+	}
+}
---
+++ Assets/scripts/WinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextControl;
+
+public class WinScript : MonoBehaviour {
+
+	private GameObject hideAtStart;
+	// Use this for initialization
+	void Start () {
+		hideAtStart = GameObject.Find ("MVP");
+		hideAtStart.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			print("enter");
+			hideAtStart.gameObject.SetActive (true);
+		}
+	}
+}
