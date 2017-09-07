5b0a5bb88326b06e731a9c0115f6b92f035d5733
David <David>
2017-09-06 21:58:41 -0700

    alittle more user friendly, adjust explosion, adjust bricks

--- Assets/scripts/TextController.cs
+++ Assets/scripts/TextController.cs
@@ -2,41 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace TextControl
 {
 	public class TextController : MonoBehaviour {
 
 		public Text text;
-		private enum States {play, win};
-		private States myState;
-
-		// Use this for initialization
-		void Start () {
-			myState = States.play;
+		void Start(){
+			text.text = "Press and Hold The Space Bar to add force to the smaller ball";
 		}
-		
-		// Update is called once per frame
-		void Update () {
-			if (myState == States.play) {
-				state_space ();
-			}else if(myState == States.win){
-				state_enter ();
+		void FixedUpdate(){
+			if (Input.GetKey (KeyCode.Space)) {
+				text.text = "Hold till you are ready to launch!";
+			}
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				text.text = "Onwards! To Try again Press 'R'";
+			}
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+			
 			}
 		}
-		public void state_space()
-		{
-			text.text = "Launch The Ball!";
-			if (Input.GetKeyDown (KeyCode.Space)) {
-				myState = States.win;
-			}
-		}
-		void state_enter()
-		{
-			text.text = "Press Enter";
-			if (Input.GetKeyDown (KeyCode.Return)) {
-				myState = States.play;
-			}
-		}
+//		private enum States {play, win};
+//		private States myState;
+//
+//		// Use this for initialization
+//		void Start () {
+//			myState = States.play;
+//		}
+//		
+//		// Update is called once per frame
+//		void Update () {
+//			if (myState == States.play) {
+//				state_space ();
+//			}else if(myState == States.win){
+//				state_enter ();
+//			}
+//		}
+//		public void state_space()
+//		{
+//			text.text = "Launch The Ball!";
+//			if (Input.GetKeyDown (KeyCode.Space)) {
+//				myState = States.win;
+//			}
+//		}
+//		void state_enter()
+//		{
+//			text.text = "Press Enter";
+//			if (Input.GetKeyDown (KeyCode.Return)) {
+//				myState = States.play;
+//			}
+//		}
 	}
 }
